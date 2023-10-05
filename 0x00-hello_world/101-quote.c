aba027a9cf52cf1d3255e010b016ea8ad43ef133
Emmanuelngato <emmanuelngato242@gmail.com>
2023-10-05 06:05:18 -0700

    C program that prints exactly and that piece of art is useful  Dora Korpar 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	fput("Write a C program that prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("Write a C program that prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
